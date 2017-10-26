555deaa91012287b75f9278ded2ca0f10489ce56
Arek Czarnoglowski <arkadiusz.czarnoglowski@pb.com>
2017-10-26 23:21:14 +0200

    Add maven and docker alias with sudo

--- jenkins-fat-bicz/build.bat
+++ jenkins-fat-bicz/build.bat
@@ -1,6 +1,6 @@
-set BUILD_NUMBER="0.0.1"
+set BUILD_NUMBER="0.0.2"
 echo Building Jenkins fat bicz docker image czarny/jenkins-fat-bicz:%BUILD_NUMBER%
 
-docker build -t czarny/fat_jenkins:%BUILD_NUMBER% .
+docker build -t czarny/jenkins-fat-bicz:%BUILD_NUMBER% .
 
-echo Jenkins++ docker image czarny/jenkins++:%BUILD_NUMBER% DONE...
+echo Jenkins fat bicz docker image czarny/jenkins-fat-bicz:%BUILD_NUMBER% DONE...
