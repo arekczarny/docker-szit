a0014909513f6c00116d5028baf3f32738c3dd40
Arek Czarnoglowski <arkadiusz.czarnoglowski@pb.com>
2017-10-27 00:19:25 +0200

    Change base docker jenkins image and clean up

--- jenkins-fat-bicz/build.bat
+++ jenkins-fat-bicz/build.bat
@@ -1,6 +1,6 @@
-set BUILD_NUMBER="0.0.2"
+set BUILD_NUMBER="0.0.1"
 echo Building Jenkins fat bicz docker image czarny/jenkins-fat-bicz:%BUILD_NUMBER%
 
-docker build -t czarny/jenkins-fat-bicz:%BUILD_NUMBER% .
+docker build --no-cache -t czarny/jenkins-fat-bicz:%BUILD_NUMBER% .
 
 echo Jenkins fat bicz docker image czarny/jenkins-fat-bicz:%BUILD_NUMBER% DONE...
